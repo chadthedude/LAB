b9a93520d77d7b82cd776875d2d4b3ce5643678e
PetetheCATLOVER <724626@student.dusd.net>
2025-05-08 11:42:12 -0700

    started programming player movement

---
+++ EscapeFromEarth/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    
+    private float horizontalInput;
+    private float forwardInput;
+    public float zRange = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the player forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+      
+       if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, -transform.position.y, -zRange);
+        }
+
+       if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+}
